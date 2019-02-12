bed7a3ebd6b225d6ed96e3c89e535d8afd6b63c7
cyril-pallasigui <46484675+cyril-pallasigui@users.noreply.github.com>
2019-02-12 19:45:40 +0800

    add connectionString

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : process.env.DATABASE_URL, // this is the add-on when you do "heroku pg:info"
+    connectionString : process.env.DATABASE_URL, // this is the add-on when you do "heroku pg:info"
     ssl: true
   }
 });
